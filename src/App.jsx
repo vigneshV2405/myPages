5cca64c49f1bc894481dcb64e0e10611fa31f7a5
vigneshV2405 <vigneshvarikolu111@gmail.com>
2023-09-06 15:42:03 +0530

    gfchjb

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './counter.jsx'
 import TodoList from './todolist.jsx'
+import axios from 'axios';
 
 function App() {
+  var [countries,setC] = React.useState([])
+  React.useEffect(()=>{
+    axios.get("https://restcountries.com/v3.1/all")
+    .then((res)=>{
+      setC(res.data)
+      console.log(res.data)
+    })
+  },[])
   return (
     <>
     <h1>Welcome</h1>
     {/* <Counter x={50}></Counter>
     <Counter x={10}></Counter> */}
-    <TodoList title={"Todo"} id={"inp1"} ar={['get clothes','buy shoes','book tickets']}></TodoList>
-    <TodoList title={"convo"} id={"inp2"} ar={['pack bags']}></TodoList>
+    {/* <TodoList title={"Todo"} id={"inp1"} ar={['get clothes','buy shoes','book tickets']}></TodoList>
+    <TodoList title={"convo"} id={"inp2"} ar={['pack bags']}></TodoList> */}
+    <ul>
+      {countries.map((country)=>{
+        return <li>{country.name.common}</li>
+      })}
+    </ul>
     </>
   )
 }
