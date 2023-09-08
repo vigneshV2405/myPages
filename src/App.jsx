b6ac80bc47914705e9f570c3d7ce42b844db57cf
vigneshV2405 <vigneshvarikolu111@gmail.com>
2023-09-08 12:19:49 +0530

    bn

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,7 @@ import axios from 'axios';
 import Child from './childComp.jsx'
 
 function App() {
-  var [x,setX] = React.useState(109)
-  var [y,setY] = React.useState(654)
-  function incx(){
-    setX(x+1)
-  }
-  function incy(){
-    setY(y+1)
-  }
-  var ar = React.useMemo(()=>{
-    return [1,2,3]
-  },[])
-  return (
-    <div>
-      <h1>Hello</h1>
-      <h1>Counter:{x}</h1>
-      <h1>Counter:{y}</h1>
-      <button onClick={()=>{incx()}}>increase X</button>
-      <button onClick={()=>{incy()}}>increase Y</button>
-      <br></br>
-      <br></br>
-      <Child x={ar}></Child>
-    </div>
-  )
+  
 }
 
 export default App;
