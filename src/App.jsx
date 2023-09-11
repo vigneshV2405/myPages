e66d757cc2ad6b6cfc7a29a90b8db7580bb9141c
vigneshV2405 <vigneshvarikolu111@gmail.com>
2023-09-11 17:43:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Counter from './counter.jsx'
-import TodoList from './todolist.jsx'
+import axios from 'axios'
+import React from 'react'
+import Countrieslist from './countries';
 
 function App() {
   return (
     <>
-    <h1>Welcome</h1>
-    <Counter x={50}></Counter>
-    <Counter x={10}></Counter>
-    <TodoList ar={['get clothes','buy shoes','book tickets']}></TodoList>
+      <Countrieslist></Countrieslist>
     </>
   )
 }
