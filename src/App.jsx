497b0185319512524528ad1e19c9d9de4021c3c1
vigneshV2405 <vigneshvarikolu111@gmail.com>
2023-09-14 17:02:58 +0530

    students cards-redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { Provider } from "react-redux";
-import store from "./store/store";
-import Counter from "./counterComp";
-import Todolist from "./todoComp";
-import CountriesComp from "./CountriesComp";
+import store from './store/store';
+import Cards from "./Cards";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="box">
-        <h1>Hello</h1>
-        <Counter></Counter>
-        <Todolist></Todolist>
-        <CountriesComp></CountriesComp>
+      <div>
+        <Cards></Cards>
       </div>
     </Provider>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,33 @@
-import {combineReducers,createStore} from 'redux'
-import counterReducer from './reducers/counterReducer'
-import todoReducer from './reducers/todoReducer'
-import CountriesReducer from './reducers/countriesReducer'
+import { combineReducers , createStore } from "redux";
+import cardsReducer from "./reducers/cardsReducer";
 
-const reducer = new combineReducers({c:counterReducer,td:todoReducer,cr:CountriesReducer})
-const store = createStore(reducer)
+const reducer = combineReducers({cr:cardsReducer})
+const store = new createStore(reducer)
 
 export default store
+
+{/* <div className='border border-3 border-danger m-3 p-3 bg-light'>
+                                    <table>
+                                        <tbody>
+                                            <tr>
+                                                <td>Firstname:</td>
+                                                <td>{student.fname}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Lastname:</td>
+                                                <td>{student.lname}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Email:</td>
+                                                <td>{student.email}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Mobile number:</td>
+                                                <td>{student.phn}</td>
+                                            </tr>
+                                            <tr>
+                                                <td colSpan={2} align={'center'}><button onClick={()=>{props.dispatch({type:'DELETEstudent',payload:i})}}>delete</button></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div> */}
