1de3142d014ee0189a8ed6f248859512272dc827
vigneshV2405 <vigneshvarikolu111@gmail.com>
2023-09-15 16:01:12 +0530

    v

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 /* import axios from 'axios';
 import Ren from "./useEffect.jsx"; */
-import StudentForm from './studentForm.jsx';
+import StudentForm from "./studentForm.jsx";
 
 function App() {
-    return (
-      <>
-        <StudentForm></StudentForm>
-      </>
-    )
+  return (
+    <>
+      <StudentForm></StudentForm>
+    </>
+  );
 }
 
 export default App;
